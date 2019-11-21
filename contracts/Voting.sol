734db5f56c05a81abb8764fd79ffbf8180136b9f
anthonybryanm.pagarigan <anthonypagarigan1105@gmail.com>
2019-11-21 13:35:00 +0800

    1st Commit

---
+++ contracts/Voting.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Voting {
+    // Store candidate
+    // Read candidate
+    string public candidate;
+    // Constructor
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
